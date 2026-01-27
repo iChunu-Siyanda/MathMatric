cc6579a9321a0f10ceaa8d237c667e7670281a2c
Siyanda Mchunu <mchunusiyanda225@gmail.com>
2026-01-27 11:05:14 -0800

    Add ExamPaperRespositoryImpl

--- lib/features/papers/data/local/exam_paper.dart
+++ lib/features/papers/data/local/exam_paper.dart
@@ -1,4 +1,5 @@
 import 'package:math_matric/features/papers/domain/entities/exam_paper.dart';
+import 'package:math_matric/features/papers/domain/entities/paper_type.dart';
 
 //Enums are much better than strings, they are safe(compile-time safety).
 enum ExamSession {
@@ -8,7 +9,16 @@ enum ExamSession {
 }
 
 class ExamPaperRepository {
-  static final Map<ExamSession, List<ExamPaper>> papers = {
+  Map<ExamSession, List<ExamPaper>> getItems(PaperType type){
+    switch (type){
+      case PaperType.paper1:
+        return _getExamPaper1();
+      case PaperType.paper2:
+        return _getExamPaper2();  
+    }
+  }
+
+  Map<ExamSession, List<ExamPaper>> _getExamPaper1 () => {
     ExamSession.june: [
       ExamPaper(
           title: "P1 June 2024",
@@ -29,5 +39,7 @@ class ExamPaperRepository {
           id: 'nov_ieb_p1_2024')
     ]
   };
+
+  Map<ExamSession, List<ExamPaper>> _getExamPaper2() => {};
 
 }
---
+++ lib/features/papers/data/respositories/exam_repository_impl.dart
@@ -0,0 +1,5 @@
+import 'package:math_matric/features/papers/data/local/exam_paper.dart';
+
+class ExamPaperRepositoryImpl implements ExamPaperRepository {
+
+}
