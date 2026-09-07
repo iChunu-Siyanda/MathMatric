7b41ad35558230d1cd0defd27dec44ab98b0bd1d
Siyanda Mchunu <mchunusiyanda225@gmail.com>
2026-09-07 10:13:33 +0200

    Update firestore security rule.

--- functions/src/shared/student_type.ts
+++ functions/src/shared/student_type.ts
@@ -1,6 +1,6 @@
 export const StudentType = {
-    pureMathsStudent: "pure_maths_student",
-    mathsLiteracyStudent: "maths_literacy_student",
+    pureMathsStudent: "pure_maths_students",
+    mathsLiteracyStudent: "maths_literacy_students",
 }
 
 export type StudentType = typeof StudentType[keyof typeof StudentType];
--- lib/core/constants/student_type.dart
+++ lib/core/constants/student_type.dart
@@ -1,6 +1,6 @@
 class StudentType {
-  static const pureMathsStudent = "pure_maths_student";
-  static const mathsLiteracyStudent = "maths_literacy_student";
+  static const pureMathsStudent = "pure_maths_students";
+  static const mathsLiteracyStudent = "maths_literacy_students";
 
   const StudentType._();
 }
